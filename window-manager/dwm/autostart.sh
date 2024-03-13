569394455ced2d241948fd46cad3138a7a1297dc
pedro-git-projects <pedro.coding.contact@gmail.com>
2024-03-13 02:06:12 -0300

    improving dwm config

--- window-manager/dwm/autostart.sh
+++ window-manager/dwm/autostart.sh
@@ -7,16 +7,10 @@ function run {
   fi
 }
 
-# Render on multiple monitors, change depending on you xrarandr -q output
-#run "xrandr --auto --output DP2-1 --mode 1920x1080 --rate 60  --right-of eDP1"
-# run "xrandr --auto --output DP2-3 --mode 1360x768 --right-of eDP1"
-# run "xrandr --auto --output DP2-1 --mode 1920x1080 --rate 60  --left-of eDP1"
-run "xrandr --auto --output DP-2-1 --mode 1920x1080 --rate 60  --left-of eDP-1"
-#run "xrandr --auto --output DP2-1 --mode 1920x1080 --rate 60  --right-of eDP1"
+run "xrandr --auto --output HDMI-2 --mode 1920x1080 --rate 60  --left-of eDP-1"
 run "nm-applet"
 run "cbatticon"
 run "xfce4-power-manager"
-#run "blueman-applet"
 run "/usr/lib/xfce4/notifyd/xfce4-notifyd"
 run "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
 picom --config ~/.config/dwm/picom.conf & 
@@ -27,6 +21,7 @@ run "wmname LG3D"
 run "unclutter"
 run keepassxc &
 run "/opt/ivpn/ui/bin/ivpn-ui"
-run "feh --bg-fill /home/pedro/Pictures/wallpapers/death.jpg --bg-fill /home/pedro/Pictures/wallpapers/sad_jesus_bigger.png"
+run "feh --bg-fill $HOME/Pictures/wallpapers/samori.jpg --bg-fill $HOME/Pictures/wallpapers/samori.jpg"
 # run setxkbmap us
-#run "spotify-tray"
+# run "spotify-tray"
+# run "blueman-applet"
